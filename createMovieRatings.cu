133485eca9157ab3a448d9aeb5d364fe96542d40
Samuel Richerd <sricherd@caltech.edu>
2015-05-24 19:09:25 -0700

    Add comments and fix bug in CMRKernel
    CMRKernel = createMovieRatingsKernel

--- createMovieRatings.cu
+++ createMovieRatings.cu
@@ -12,9 +12,11 @@ createMovieRatingsKernel(const float *weights, const float *hidden_features,
 
     // weights[NUM_MOVIES][5][NUM_FEATURES]
     //
+    // movie_rating_index = movie_id * 5 + rating_id
+    //      (index of current movie_id/rating_id pair)
     unsigned int movie_rating_index = blockIdx.x * blockDim.x + threadIdx.x;
     unsigned int i = 0;
-    float dot_prod;
+    float dot_prod; // temporary, local dot product variable
     while (movie_rating_index < num_movies * 5) {
         dot_prod = 0.00; // Initialize the dot product to 0
 
@@ -24,9 +26,12 @@ createMovieRatingsKernel(const float *weights, const float *hidden_features,
             // rating_id - [0, 4]
             // feature_id - [0, 99]
             dot_prod += weights[movie_rating_index*num_hidden_features + i]
-                        + hidden_features[i]; // Do the dot product
+                        * hidden_features[i]; // Do the dot product
         }
+        // store the dot_product result
         movie_rating_probs[movie_rating_index] = dot_prod;
+
+        // re-use this thread on another data point:
         movie_rating_index += blockDim.x * gridDim.x;
     }
 }
