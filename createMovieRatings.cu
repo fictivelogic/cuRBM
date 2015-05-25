ab3f89130f2114c085fe89133d3f0a4204370531
Samuel Richerd <sricherd@caltech.edu>
2015-05-24 18:57:50 -0700

    Create first-pass createMovieRatingsKernel
    Not yet tested, but committed for reference.

---
+++ createMovieRatings.cu
@@ -0,0 +1,28 @@
+//
+// Created by samuel on 5/24/15.
+//
+
+#include <cuda_runtime.h>
+#include "createMovieRatings.h"
+
+
+__global__ void
+createMovieRatingsKernel(const float *weights, const float *hidden_features,
+    float* movie_rating_probs, int num_movies, int num_hidden_features) {
+
+    // weights[NUM_MOVIES][5][NUM_FEATURES]
+    //
+    unsigned int movie_rating_index = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int i = 0;
+    float dot_prod;
+    while (movie_rating_index < num_movies * 5) {
+        dot_prod = 0.00; // Initialize the dot product to 0
+
+        for (i = 0; i < num_hiddens; i++) {
+            dot_prod += weights[movie_rating_index*num_hidden_features + i]
+                        + hidden_features[i];
+        }
+        movie_rating_probs[movie_rating_index] = dot_prod;
+        movie_rating_index += blockDim.x * gridDim.x;
+    }
+}
