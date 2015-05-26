c1fb5d71c8ad4b4bbba14e3b538d7fdc6d264aec
obinnae <binnixe@gmail.com>
2015-05-25 19:51:37 -0700

    non-optimized version of initial hiddens generation

--- genereate_hidden.cu
+++ genereate_hidden.cu
@@ -7,23 +7,33 @@
 #define NUM_MOVIES 17770
 #define NUM_USERS 458293
 
+
+float sigmoid (float input); //TODO: Log probability?
+
 __global__
 void createInitialHidden(const int * const train_points,
 						 const int * user_start,
 						 const int * user_length,
+						 const int * const b_hid,
+						 float * intial_hiddens,
 						 int num_hidden,
 						 float * output) {
 
   // TODO: do not modify code, just comment on suboptimal accesses
+	//
 
-	int i_start = user_start(threadIdx.x + (blockDim.x * threadIdx.y));
+	int i_start = user_start[threadIdx.x + (blockDim.x * threadIdx.y)];
+	int i_length = user_length[threadIdx.x + (blockDim.x * threadIdx.y)];
+	//Need to know what size the thread block is so we can determine how many users each thread should attend to
 
-
-	const int i = threadIdx.x + 64 * blockIdx.x;
-	int j = 4 * threadIdx.y + 64 * blockIdx.y;
-	const int end_j = j + 4;
-
-	for (; j < end_j; j++) {
-	  output[j + n * i] = input[i + n * j];
+	for (int i = 0; i < num_hidden < i++) //100 loops
+	{
+		float dot_prod = 0;
+		for (int m = 0; m < i_length; m++)
+		{
+			int rating = train_points[i_start + (m * 2)];
+			dot_prod += W[movie][rating][i];
+			intial_hiddens[i] = sigmoid(dot_prod - b_hid[i]);
+		}
 	}
 }
