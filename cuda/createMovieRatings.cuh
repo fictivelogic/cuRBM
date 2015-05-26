82b8f93ebfcafb945985811d3261e09df2c206fe
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-05-25 18:09:14 -0700

    Refactor 'hidden units' to 'hidden features'

--- cuda/createMovieRatings.cuh
+++ cuda/createMovieRatings.cuh
@@ -2,7 +2,7 @@
 #define NETFLIX_RBM_CREATEMOVIERATINGS_H
 
 // Second step of RBM
-// Get movie rating probabilities from initial hidden unit probabilities
+// Get movie rating probabilities from initial hidden feature probabilities
 __global__ void createMovieRatingsKernel(const float *weights,
                                          const float *initial_hidden_features,
                                          float* movie_rating_probs,
